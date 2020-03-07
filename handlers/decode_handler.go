8b7881b9d931c95c61eb7d60c49410513d6946f2
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-07 16:23:20 -0500

    Implement decode handler

--- handlers/decode_handler.go
+++ handlers/decode_handler.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/dpgil/url-shortener/types"
 )
 
 // Decode takes a short URL and returns the long URL.
@@ -17,13 +20,30 @@ func Decode(db *dynamodb.DynamoDB, tableName string) func(w http.ResponseWriter,
 			return
 		}
 
-		// check if the short URL is in the mapping
-		longURL, ok := urlMapping[shortURL]
-		if !ok {
-			http.Error(w, fmt.Sprintf("short url %s does not exist", shortURL), http.StatusBadRequest)
+		// check if the short url is in the database
+		params := &dynamodb.GetItemInput{
+			TableName: aws.String(tableName),
+			Key: map[string]*dynamodb.AttributeValue{
+				"shortLink": {
+					S: aws.String(shortURL),
+				},
+			},
+		}
+
+		resp, err := db.GetItem(params)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("short link %s not found in database", shortURL), http.StatusNotFound)
 			return
 		}
 
-		fmt.Fprintf(w, longURL)
+		// unmarshal the dynamodb attribute values into our struct
+		var mapping types.Mapping
+		err = dynamodbattribute.UnmarshalMap(resp.Item, &mapping)
+		if err != nil {
+			http.Error(w, "error unmarshaling response from dynamo", http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, mapping.LongLink)
 	}
 }
