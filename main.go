8b6924c391fc28f6b7b5338083610bbb1de5b47e
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-07 17:05:28 -0500

    Smarter config

--- main.go
+++ main.go
@@ -19,19 +19,25 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	// read config properties
-	config, err := ioutil.ReadFile(".config")
-	if err != nil {
-		log.Fatal("No config file found. Expected file named .config with dynamo endpoint, region, and table name.", err)
-	}
-	configParams := strings.Split(string(config), "\n")
-	if len(configParams) < 3 {
-		log.Fatal("Missing config parameters")
-	}
+	endpoint := os.Getenv("DYNAMO_ENDPOINT")
+	region := os.Getenv("DYNAMO_REGION")
+	tableName := os.Getenv("DYNAMO_TABLE_NAME")
 
-	endpoint := configParams[0]
-	region := configParams[1]
-	tableName := configParams[2]
+	if endpoint == "" || region == "" || tableName == "" {
+		// No config environment variables set. Check if a .config file exists.
+		config, err := ioutil.ReadFile(".config")
+		if err != nil {
+			log.Fatal("No config file found. Expected file named .config with dynamo endpoint, region, and table name.", err)
+		}
+		configParams := strings.Split(string(config), "\n")
+		if len(configParams) < 3 {
+			log.Fatal("Missing config parameters")
+		}
+
+		endpoint = configParams[0]
+		region = configParams[1]
+		tableName = configParams[2]
+	}
 
 	// create an aws session
 	sess := session.Must(session.NewSession(&aws.Config{
@@ -46,7 +52,7 @@ func main() {
 	http.HandleFunc("/e/", handlers.Encode(db, tableName))
 	http.HandleFunc("/r/", handlers.Redirect(db, tableName))
 
-	err = http.ListenAndServe(":"+port, nil) // set listen port
+	err := http.ListenAndServe(":"+port, nil) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
