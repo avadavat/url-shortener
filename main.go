d227280d88da500d73b2da2b768e23949e3241f6
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-07 13:19:59 -0500

    Working handlers

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"strings"
 )
 
-func doSomething(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       // parse arguments, you have to call this by yourself
+func redirect(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // parse arguments, you have to call this by yourself
+
+	// todo: delete this block
 	fmt.Println(r.Form) // print form information in server side
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
@@ -17,11 +19,22 @@ func doSomething(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "Hello danl!") // send data to client side
+
+	fmt.Fprintf(w, "redirect") // send data to client side
+}
+
+func encode(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "encode")
+}
+
+func decode(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "decode")
 }
 
 func main() {
-	http.HandleFunc("/", doSomething)
+	http.HandleFunc("/decode/", decode)
+	http.HandleFunc("/encode/", encode)
+	http.HandleFunc("/", redirect)
 	err := http.ListenAndServe(":9090", nil) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
